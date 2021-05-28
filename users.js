75b0a2c31b3bab9b6903b23a8e440f5d03113bbe
Peggy <peggymailula@gmail.com>
2021-05-28 15:48:59 +0200

    upload

---
+++ users.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
+    // get a reference to the template script tag
+    var templateSource = document.querySelector(".userTemplate").innerHTML;
+    // compile the template
+    var userTemplate = Handlebars.compile(templateSource);
+    // pass the data into the template & get the HTML back
+    var userDataElem = document.querySelector(".userData");
+    // get a reference to the element in the DOM where we would like to display the data
+    var userData = { users: [
+        {username: "alan", firstName: "Alan", lastName: "Johnson", email: "alan@test.com" },
+        {username: "allison", firstName: "Allison", lastName: "House", email: "allison@test.com" },
+        {username: "ryan", firstName: "Ryan", lastName: "Carson", email: "ryan@test.com" }
+      ]};
+      var userDataHTML = userTemplate(userData);
+      userDataElem.innerHTML = userDataHTML;
+    // put the resulting HTML into the target elements innerHTML
+});
